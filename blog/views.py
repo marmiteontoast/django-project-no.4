9bac8e2a37da3915283a2ce4531250e2fc7ff2c8
mark john <mark.john@editmatters.co.uk>
2022-09-15 05:56:02 +0000

    created base.html file and updated home and about page

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Destiny Franks',
+        'title': 'Blog Post 1',
+        'content': 'This is my first blog post',
+        'date_posted': '7th August 2021',
+    },
+
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'This is my second blog post',
+        'date_posted': '14th August 2021',
+    }
+]
+
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': "About Page"})
