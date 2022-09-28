fa91d4ee12907a9cfd974613c7e1cc0b7e95a2e2
mark john <mark.john@editmatters.co.uk>
2022-09-28 03:38:40 +0000

    users are able to create posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from blog import views 
+from .views import PostListView, PostDetailView, PostCreateView
 
 urlpatterns = [
-    path('', views.home, name="blog-home"),
+    # path('', views.home, name="blog-home"),
     path('about/', views.about, name="blog-about"),
+
+    path('', PostListView.as_view(), name="blog-home"),
+    path('post/new', PostCreateView.as_view(), name="blog-new"),
+    path('post/<int:pk>/', PostDetailView.as_view(), name="blog-detail")
 ]
