d9b231dee2fce124598556b916bf9c13fdbf2f74
mark john <mark.john@editmatters.co.uk>
2022-10-09 08:48:54 +0000

    change setting.py for cloudinary again and renamed main.css sheet to style.css

--- blogproj/settings.py
+++ blogproj/settings.py
@@ -27,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["blogproj-project-4.herokuapp.com", "localhost"]
 
@@ -142,7 +142,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-
 
 
 # Default primary key field type
